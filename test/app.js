7b3a868e2d5adea4d19b5a4427bd535ec3339f44
lvxiaoyang <lvxiaoyang@ktplay.com>
2014-12-02 19:07:10 +0800

    ver 0.3

--- test/app.js
+++ test/app.js
@@ -1,9 +1,12 @@
 //
 /////////////////////////////////
-var HttpConn = require('../httpConn').create();
+var HttpConn = require('../').create();
 HttpConn.start(3000, '127.0.0.1');
 HttpConn.on('conn', function(uid, queryObj, data){
     console.log('event_conn:', JSON.stringify(arguments));
+    setTimeout(function(){
+        HttpConn.SendByUid(uid, 'hello,world');
+    }, 3000);
 });
 HttpConn.on('close', function(uid){
     console.log('event_close:', JSON.stringify(arguments));
